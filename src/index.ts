421bef1a1d666061c230e5d5a53d347b570f54e0
gary <gary@ligginsdevelopment.com>
2022-12-30 19:36:41 -0800

    added map and encapsulated the map info

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('hi there');
+// /// <reference types="@types/google.maps" />
+import {User} from './User';
+import { Company } from './Company';
+import { CustomMap } from "./CustomMap";
+
+//creating a instance of a user
+const user = new User();
+console.log(user);
+
+//creating a instance of a company
+const company = new Company();
+console.log(company);
+
+//creating a instance of a google map
+new CustomMap("map");
